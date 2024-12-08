07fc64350d974e10c67a5accc9519730b39b8a7a
Daniel Hennigar <danrhennigar@gmail.com>
2024-12-08 10:27:18 -0800

    update license info

--- bin/sway-run-or-raise.pl
+++ bin/sway-run-or-raise.pl
@@ -4,27 +4,26 @@
 
 # Copyright (c) 2024 Daniel Hennigar
 
-# This program is dual-licensed under the GNU General Public License, version 3, or
-# the Artistic License, version 2.0. You may choose to use, modify, and redistribute
-# it under either of these licenses.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of either:
+#
+#   a) the Artistic License 2.0, or
+#   b) the GNU General Public License as published by the Free Software
+#      Foundation; either version 3, or (at your option) any later version.
+#
+# See the LICENSE file for more information.
 
 use 5.032;
 
-# Check arguments
 if (@ARGV < 2) {
     die "Usage: $0 <app_id or class> <command>\n";
 }
-
 my ($identifier, $command) = @ARGV;
 
-# Try focusing the app using app_id
 my $focus_by_app_id = system("swaymsg", "[app_id=$identifier] focus") == 0;
-
-# If focusing by app_id failed, try focusing by class
 unless ($focus_by_app_id) {
     my $focus_by_class = system("swaymsg", "[class=$identifier] focus") == 0;
 
-    # If both attempts failed, execute the command
     unless ($focus_by_class) {
         system("swaymsg", "exec", $command);
     }
